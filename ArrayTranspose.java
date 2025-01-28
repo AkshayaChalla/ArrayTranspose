cd3e560194d6308a560aecd5a209092af667b756
AkshayaChalla <akshaya.challa215@gmail.com>
2025-01-28 10:26:51 +0530

    Add files via upload

---
+++ ArrayTranspose.java
@@ -0,0 +1,45 @@
+public class ArrayTranspose  {
+    public static void main(String[] args) {
+        // Sample 2D array
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+        int[][] transposedMatrix = transpose(matrix);
+        
+
+        System.out.println("\nTransposed Matrix:");
+        printMatrix(transposedMatrix);
+    }   
+
+    public static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposed = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        return transposed;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+    
